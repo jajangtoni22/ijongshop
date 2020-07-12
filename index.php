9629f4966ed089adc68ade1a82e57b03a79ea0c7
jajangtoni22 <62067411+jajangtoni22@users.noreply.github.com>
2020-07-13 00:41:22 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+	session_start();
+
+	require "./includes/koneksi.php";
+	require "./includes/lib.php";
+	require "./includes/fungsi.php";
+	require "./includes/title.php";
+	require "./includes/header.php";
+	require "./includes/visitor.php";
+	require "./includes/polling.php";
+	require "./module/rss.php";
+
+	require "".ft."index.php";
+	require "./includes/seo.php";
+
+
+?> 
